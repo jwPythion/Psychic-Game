377d484e54a4ac6867a4098c437973249c990e1c
jwpythion <jaymesjohnson@me.com>
2018-06-14 09:54:38 -0700

    Got the win/lose logic working, computer picks a random letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,15 @@
-var cleoChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var cleoChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 var wins = 0;
 var losses = 0;
 var userGuesses = 9;
 
+
+// computer picks a random letter 
+// from the cleoChoices array
+var cleoChosenLetter = cleoChoices[Math.floor(Math.random()*cleoChoices.length)];
+console.log(cleoChosenLetter);
 
 
 //will need to use document.onkeyup!
@@ -14,20 +19,22 @@ document.onkeyup = function (event) {
     var userGuess = event.key; //??
     console.log(userGuess);
 
-    // computer picks a random letter 
-    // from the cleoChoices array
+
 
     // user select a letter 
 
-    // compare the cleoChoice letter with the user selected 
-
-    // if they match 
-    // user wins 
-
-    // if they don't match 
-    // losses go up by one 
-    // userGuesses go down by one
-
+    // compare the cleoChoice letter with the user selected
+    if(userGuess === cleoChosenLetter) {
+        // if they match 
+        // user wins 
+        alert("You win!");
+    } else {
+        // if they don't match 
+        // losses go up by one 
+        // userGuesses go down by one
+        alert("You lose!");
+    }
+ 
 }
 
 
