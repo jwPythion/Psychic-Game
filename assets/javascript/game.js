afed64c7e50f39ae086f9bf76aef3055a49d0654
jwpythion <jaymesjohnson@me.com>
2018-06-15 18:34:11 -0700

    updated final bits of functional psychic game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+
+
+
+
 var cleoChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
@@ -8,33 +12,35 @@ var userGuesses = 9;
 
 // computer picks a random letter 
 // from the cleoChoices array
-var cleoChosenLetter = cleoChoices[Math.floor(Math.random()*cleoChoices.length)];
-console.log(cleoChosenLetter);
+var cleoChosenLetter = cleoChoices[Math.floor(Math.random() * cleoChoices.length)];
+console.log(cleoChosenLetter);//check that this is working...
 
 
-//will need to use document.onkeyup!
+//will need to use document.onkeyup to track!
 
 document.onkeyup = function (event) {
 
-    var userGuess = event.key; //??
-    console.log(userGuess);
+    var userGuess = event.key; 
+    console.log(userGuess);//check that this is working with consolelog
 
 
 
-    // user select a letter 
+    // user selects a letter...
 
     // compare the cleoChoice letter with the user selected
-    if(userGuess === cleoChosenLetter) {
+    if (userGuess === cleoChosenLetter) {
         // if they match 
-        // user wins 
+        // user wins & #of wins increases by one
+        wins++
         alert("You win!");
     } else {
         // if they don't match 
         // losses go up by one 
         // userGuesses go down by one
+        losses++
         alert("You lose!");
     }
- 
+
 }
 
 
